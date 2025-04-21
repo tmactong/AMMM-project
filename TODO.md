57286ecad834ba110d066bdafc1e965848cad198
tmactong <tracymcgradytong@gmail.com>
2025-04-21 23:13:18 +0200

    grasp method

---
+++ TODO.md
@@ -0,0 +1,2 @@
+
+- [ ] Local search Optimization
--- src/project/cmd/solve.py
+++ src/project/cmd/solve.py
@@ -14,13 +14,13 @@ if __name__ == '__main__':
     # project 2: 6 members
     # main('../testdata/project.2.dat', 'greedy')
     # main('../testdata/project.2.dat', 'local_search')
-    main('../testdata/project.2.dat', 'grasp', alpha=0.9)
+    # main('../testdata/project.2.dat', 'grasp', alpha=0.9)
     # project 3: 10 members
     # main('../testdata/project.3.dat', 'greedy')
     # project 4: 10 members
     # main('../testdata/project.4.dat', 'greedy')
     # main('../testdata/project.4.dat', 'local_search', '../result/greedy/project.4.json')
-    # main('../testdata/project.4.dat', 'grasp', alpha=0.5)
+    main('../testdata/project.4.dat', 'grasp', alpha=0.5)
     # main('../testdata/project.4.dat', 'local_search', '../result/greedy/project.4.ls_1st.json')
     # test infeasible solution
     # main('../testdata/infeasible_solution.dat', 'greedy')
